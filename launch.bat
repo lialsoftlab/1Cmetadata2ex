f5155a93b51f3180a298faf238b444e224b729d9
Alexey V. Litvinov <litvinov_av@ipiton.ru>
2019-07-10 14:59:30 +1000

    Comment on using 32-bit connector on 64-bit system

--- launch.bat
+++ launch.bat
@@ -1,1 +1,5 @@
+REM needed for start 32-bit cscript/wscript on 64-bit system else not working with 32-bit COMConnector
+REM or else you need to patch 1C COM connector registration in registry according with instructions here
+REM https://www.codeproject.com/Tips/267554/Using-32-bit-COM-Object-from-64-bit-Application
+ 
 %SystemRoot%\SysWOW64\cscript.exe %1 %2 %3 %4 %5 
