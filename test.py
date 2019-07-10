80140bb5cf7e9b2698e8d3a9ff69d0372ef701ca
Alexey V. Litvinov <litvinov_av@ipiton.ru>
2019-07-10 16:45:21 +1000

    Загрузка конфигурации из .toml. Немного изменена сортировка значений результата.

--- test.py
+++ test.py
@@ -1,5 +1,7 @@
 import sys
+import pprint
 
+import toml
 import win32com.client as com
 
 from huepy import red, green, grey, cyan
@@ -7,6 +9,8 @@ from ruamel.yaml import YAML
 
 # monkey patch для поддержки русского языка в идентификаторах методов иначе win32com.client.build.MakePublicAttributeName() их вырезает.
 com.build.valid_identifier_chars += 'АБВГДЕЁЖЗИЙКЛМНОПРСТУФХЦЧШЩЪЫЬЭЮЯабвгдеёжзийклмнопрстуфхцчшщъыьэюя'
+
+config = toml.load("export.toml")
 
 sys.stderr.write(grey("Загрузка COM connector v8.3 ... "))
 sys.stderr.flush()
@@ -19,7 +23,7 @@ sys.stderr.flush()
 sys.stderr.write(grey("Соединяемся с 1С ... "))
 sys.stderr.flush()
 
-db1c = v83cc.Connect("Srvr=\"192.168.6.93\";Ref=\"Trade_EP\";")
+db1c = v83cc.Connect(config["connection_string"])
 
 sys.stderr.write(green("OK\n"))
 sys.stderr.flush()
@@ -28,10 +32,8 @@ sys.stderr.write(grey("Получаем структуру хранения ба
 sys.stderr.flush()
 
 objects = db1c.NewObject("Массив")
-# objects.Add("Справочник.Товары")
-# objects.Add("Справочник.Единицы")
-# objects.Add("Документ.Инвойс")
-# objects.Add("Документ.ЗаказПоставщику")
+for metaobj in config.get("metaobjects", []):
+    objects.Add(metaobj)
 
 result_kvt = db1c.ПолучитьСтруктуруХраненияБазыДанных(objects, True)
 
@@ -53,11 +55,15 @@ for line in result_kvt:
         fields.append(d)
         sys.stderr.write(grey(f"\tПоле: {cyan(field.ИмяПоляХранения)}\n"))
 
-    fields = sorted(fields, key=lambda x: x.get("FLD_META", '.') + x.get("FLD_1CQL", '.'))
+    fields = sorted(fields, key=lambda x: x.get("FLD_META", '_') + x.get("FLD_1CQL", '_'))
     
-    result.append(dict(TAB_DBMS=line.ИмяТаблицыХранения, TAB_1CQL=line.ИмяТаблицы, TAB_META=line.Метаданные, TAB_CLASS=line.Назначение, TAB_FIELDS=fields))
+    d = dict(TAB_DBMS=line.ИмяТаблицыХранения, TAB_CLASS=line.Назначение)
+    if line.ИмяТаблицы: d["TAB_1CQL"] = line.ИмяТаблицы
+    if line.Метаданные: d["TAB_META"] = line.Метаданные
+    if fields: d["TAB_FIELDS"] = fields 
+    result.append(d)
     sys.stderr.flush()
 
 
-YAML().dump(sorted(result, key=lambda x: x['TAB_DBMS']), sys.stdout)
+YAML().dump(sorted(result, key=lambda x: x.get("TAB_META", '_') + x.get("TAB_1CQL", '_') + x['TAB_DBMS']), sys.stdout)
 
